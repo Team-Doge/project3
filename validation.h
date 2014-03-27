c11d97a346ab01d9e3d7880dd29bb976a04e9316
Josh Caron <jcaron@ccs.neu.edu>
2014-03-26 21:12:00 -0400

    Comments ftw

--- validation.h
+++ validation.h
@@ -9,11 +9,27 @@
 #include "datatypes.h"
 #include "validation.c"
 
-
-
+/**
+ * Checks to see if the given IP is valid. There should be four parts,
+ * each separated by a '.', and each number is valid from 0 to 255, inclusive.
+ * @param  ip The IP address to validate
+ * @return    True if it is valid, false otherwise
+ */
 bool validate_ip_addr(char *ip);
 
+/**
+ * Checks to see if the given port is in range. Valid port range is from
+ * 0 to 65535, inclusive.
+ * @param  port The port, as a string, to check
+ * @return      True if it is, false otherwise.
+ */
 bool validate_port(char *port);
+
+/**
+ * Validate a full IP address, including a port number (optional)
+ * @param  ip The IP address to check
+ * @return    True if all parts are valid, false otherwise
+ */
 bool validate_ip(char *ip);
 
 #endif
